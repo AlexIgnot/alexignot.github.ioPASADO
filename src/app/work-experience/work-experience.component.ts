d4a58d8996150e7961c45efce71e581cead57fea
AlexIgnot <alex.ignot@hotmail.com>
2023-09-08 09:10:01 -0600

    Work experience first update

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class WorkExperienceComponent {
 
-}
+  workExperience : Array<any> = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    let work1 = {
+      fecha: "2019 - 2020",
+      ubicacion: "Orizaba, Veracruz",
+      puesto: "Desarrollador Web",
+      empresa: "Sistemas y Servicios de Veracruz",
+      logros: [
+        { descripcion: "Creación de aplicaciones web con Angular 8 y Angular Material." },
+        { descripcion: "Creación de aplicaciones web con ASP.NET Core MVC." },
+      ] 
+    };
+
+    let work2 = {
+      fecha: "2019 - 2020",
+      ubicacion: "Orizaba, Veracruz",
+      puesto: "Desarrollador Web",
+      empresa: "Sistemas y Servicios de Veracruz",
+      logros: [
+        { descripcion: "Creación de aplicaciones web con Angular 8 y Angular Material." },
+        { descripcion: "Creación de aplicaciones web con ASP.NET Core MVC." },
+      ] 
+    };
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
+
+
+  };
+    
+
