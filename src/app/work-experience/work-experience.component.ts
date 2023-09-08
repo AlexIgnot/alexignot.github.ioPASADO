ec6ef5cfd96d06124cebffbd6749714044b933d1
AlexIgnot <alex.ignot@hotmail.com>
2023-09-08 10:17:37 -0600

    services created & WorkExperience.spec updated

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,44 +1,22 @@
 import { Component } from '@angular/core';
+import { WorkExperienceService } from '../services/work-experience.service'; 
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.css']
 })
-export class WorkExperienceComponent {
+export class WorkExperienceComponent implements OnInit {
 
   workExperience : Array<any> = [];
 
-  constructor() {}
-
   ngOnInit(): void {
+    this.workExperience = this.workExperienceService.getWorkExperience()
+    .subscribe((data: any[]) => {
 
-    let work1 = {
-      fecha: "2019 - 2020",
-      ubicacion: "Orizaba, Veracruz",
-      puesto: "Desarrollador Web",
-      empresa: "Sistemas y Servicios de Veracruz",
-      logros: [
-        { descripcion: "Creación de aplicaciones web con Angular 8 y Angular Material." },
-        { descripcion: "Creación de aplicaciones web con ASP.NET Core MVC." },
-      ] 
-    };
+      console.log(data);
+      this.workExperience = data;
 
-    let work2 = {
-      fecha: "2019 - 2020",
-      ubicacion: "Orizaba, Veracruz",
-      puesto: "Desarrollador Web",
-      empresa: "Sistemas y Servicios de Veracruz",
-      logros: [
-        { descripcion: "Creación de aplicaciones web con Angular 8 y Angular Material." },
-        { descripcion: "Creación de aplicaciones web con ASP.NET Core MVC." },
-      ] 
-    };
-
-    this.workExperience.push(work1);
-    this.workExperience.push(work2);
-
-
-  };
-    
-
+    })
+  } 
+}
